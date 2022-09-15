d41730448e626f23f63e1083d08b73373449176c
redline11111 <handoinilya228@mail.ru>
2022-09-15 15:36:57 +0900

    add menu and list

--- clicker.js
+++ clicker.js
@@ -1,8 +1,22 @@
 let score = 1000;
 let updating = 0.01;
-console.log(1)
-function clickBtn() {
+let menu = document.getElementsByName("menu-item");
+let menuList = document.getElementsByName("list");
+
+document.getElementsByName("Click")[0].addEventListener('click', addScore);
+
+function addScore() {
 	score = score + updating;
-	document.getElementsByName("score")[0].innerHTML = score.toFixed(2) + " rc";
+	document.getElementsByName("score")[0].innerHTML = score.toFixed(2) + " Rc-клеток";
 }
-document.getElementsByName("Click")[0].addEventListener('click', clickBtn);
+
+function selectMenuItem(number) {
+	for (let i = 0; i < menu.length; i++) {
+		menu[i].className = '';
+		menuList[i].style.display = 'none';
+	}
+	menu[number].className = "underline decoration-rose-700 decoration-2 font-semibold text-rose-700";
+	menuList[number].style.display = '';
+}
+
+selectMenuItem(2);
