821bbcc85e1ab49ecbb21ebb4499cbb7f9362186
redline11111 <handoinilya228@mail.ru>
2022-09-18 14:00:26 +0900

    add some players functions

--- clicker.js
+++ clicker.js
@@ -1,12 +1,54 @@
 let score = 1000;
-let updating = 0.01;
 let menu = document.getElementsByName("menu-item");
 let menuList = document.getElementsByName("list");
 
-document.getElementsByName("Click")[0].addEventListener('click', addScore);
+let characteristics = {
+	strength: {
+		count: 1,
+		change(number){
+			this.count += number;
+			document.getElementsByName("strength")[0].innerHTML = 'Сила: ' + this.count;
+		}
+	},
+	agility: {
+		count: 1,
+		change(number){
+			this.count += number;
+			document.getElementsByName("agility")[0].innerHTML = 'Ловкость: ' + this.count;
+		}
+	},
+	intelligence: {
+		count: 1,
+		change(number){
+			this.count += number;
+			document.getElementsByName("intelligence")[0].innerHTML = 'Интеллект: ' + this.count;
+		}
+	},
+	endurance: {
+		count: 1,
+		change(number){
+			this.count += number;
+			document.getElementsByName("endurance")[0].innerHTML = 'Выносливость: ' + this.count;
+		}
+	},
+	charisma:{
+		count: 1,
+		change(number){
+			this.count += number;
+			document.getElementsByName("charisma")[0].innerHTML = 'Харизма: ' + this.count;
+		}
+	},
+	mastery: {
+		count: 1,
+		change(number){
+			this.count += number;
+			document.getElementsByName("mastery")[0].innerHTML = 'Мастерство: ' + this.count;
+		}
+	},
+}
 
-function addScore() {
-	score = score + updating;
+function addScore(upd) {
+	score = score + upd;
 	document.getElementsByName("score")[0].innerHTML = score.toFixed(2) + " Rc-клеток";
 }
 
@@ -19,4 +61,18 @@ function selectMenuItem(number) {
 	menuList[number].style.display = '';
 }
 
-selectMenuItem(2);
+function drinkCoffe(){
+	addScore(1)
+}
+
+function hunting(){
+	const randomRc = Math.floor(Math.random() * 10);
+	addScore(randomRc);
+}
+
+function attackGhoul(){
+	characteristics.strength.change(1);
+	const randomRc = Math.floor(Math.random() * (120 - 15) + 10);
+	addScore(randomRc);
+}
+selectMenuItem(0);
